c0d63db7d84955847160b123bf8e5fcfa7b4675c
masoud <masoud.elmashay@gmail.com>
2025-09-30 10:08:44 +0200

    add company structure feature

---
+++ app/Models/CompanyStructure.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyStructure extends Model
+{
+    protected $fillable = [
+        'structure',
+        'company_id',
+    ];
+}
